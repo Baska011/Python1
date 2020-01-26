975588ccb69ed422d9153d323f818b56ebe163b9
Baska <baska011@yahoo.com>
2020-01-26 14:06:55 +0800

    if

--- app.py
+++ app.py
@@ -8,15 +8,15 @@
 # print("urt:"len(owog))
 # urt olno.
 
-owog="BAATAR"
-print(owog.lower())
-print(owog.replace("A","o"))
-print(owog.capitalize())
-print("urt:",   len(owog))
-# print(find())  - haigaad bairlal zaana, baihgui bol -1 utga zaana.
-print(owog.find("AR"))
-print(owog.search("AR"))
-print(owog.find("Z"))
+# owog="BAATAR"
+# print(owog.lower())
+# print(owog.replace("A","o"))
+# print(owog.capitalize())
+# print("urt:",   len(owog))
+# # print(find())  - haigaad bairlal zaana, baihgui bol -1 utga zaana.
+# print(owog.find("AR"))
+# print(owog.search("AR"))
+# print(owog.find("Z"))
 
 # huvilbar udirdah system:
 
@@ -27,3 +27,28 @@ print(owog.find("Z"))
 # git -g www.git-scm.com -ruu orj pc deeree suulgaad, run-s git bash -ruu nevterch orood,
 # git -g havtasan deeree idevhjuulie. terminal deeree "git init" geed darna. 
 # Bas uur havtas nemehiin tuld zuun deed taliin Message deer "version 1" gej bicheed ctrl+enter daraa
+# cdcfdcvfd
+# print(ovog.object)
+
+# github.com
+# uuriin gesen hayag neej avah, claud tai ijil.
+# git bash --> \git push -u origin master
+
+# IF FOR WHILE
+# a=6
+# b=2
+# if a>b:
+#     print("a ni ih")
+# else:
+#     print("b ni baga")
+
+# a=int(input("a too: "))
+# b=int(input("b too: "))
+
+# if a<100:
+#     print("a too 100s baga")
+# elif a<1000:
+#     print("a too 1000s baga")
+# else:
+#     print("a too 1000s ih")
+
---
+++ if.py
@@ -0,0 +1,18 @@
+
+
+# a=int(input("a too: "))
+# b=int(input("b too: "))
+
+# if a<100:
+#     print("a too 100s baga")
+# elif a<1000:
+#     print("a too 1000s baga")
+# else:
+#     print("a too 1000s ih")
+
+a=int(input("a too: "))
+
+if a<100 and a>9: # and, or, not
+    print("2 orontoi too")
+else:
+    print("2 orontoi bish")
