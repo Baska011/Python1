b1dbb3aa31d5b4d4613091c4757614f7d6d0146e
Baska <baska011@yahoo.com>
2020-01-26 12:40:19 +0800

    version

---
+++ app.py
@@ -0,0 +1,29 @@
+# a=input int ("a too")
+# b=input("b too")
+
+# str
+#len() -urt , lower() jijig, upper() - tom, 
+# capitallize() - ehnii useg tom, replace() - temdegtiig solih
+# python string -s busad tfunkts harj bolno.
+# print("urt:"len(owog))
+# urt olno.
+
+owog="BAATAR"
+print(owog.lower())
+print(owog.replace("A","o"))
+print(owog.capitalize())
+print("urt:",   len(owog))
+# print(find())  - haigaad bairlal zaana, baihgui bol -1 utga zaana.
+print(owog.find("AR"))
+print(owog.search("AR"))
+print(owog.find("Z"))
+
+# huvilbar udirdah system:
+
+# 1.0
+# 1.2
+# 2.0
+# 2.1 n geegdtsen. iim asuudal garval tugeemel ashiglagddag. "git" -g ashiglaj uzii.
+# git -g www.git-scm.com -ruu orj pc deeree suulgaad, run-s git bash -ruu nevterch orood,
+# git -g havtasan deeree idevhjuulie. terminal deeree "git init" geed darna. 
+# Bas uur havtas nemehiin tuld zuun deed taliin Message deer "version 1" gej bicheed ctrl+enter daraa
